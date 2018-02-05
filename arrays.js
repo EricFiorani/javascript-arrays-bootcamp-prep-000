217deb6864b69cb3471f406971671b6824506b78
Eric Fiorani <efiorani305@gmail.com>
2018-02-05 16:29:21 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,12 @@
+var chocolateBars [
+  "snickers",
+  "hundred grand",
+  "kitkat",
+  "skittles"
+]
+
+function addElementToBeginningOfArray(){
+  var cities = ["New York", "San Francisco"]
+  cities.unshift("Philadelphia")
+  return cities
+}
