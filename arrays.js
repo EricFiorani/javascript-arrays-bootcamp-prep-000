5508bee9926abe77f198690aa93cad9030ff8b4b
Eric Fiorani <efiorani305@gmail.com>
2018-02-05 16:36:15 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,9 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 
 function addElementToBeginningOfArray(array, element){
-  var myArray = ["foo"]
-  myArray.push(1)
-  return myArray
+  return [element, ...array];
+}
+
+destructivelyAddElementToBeginningOfArray(array, element){
+  var myArray =
 }
